d7a9ec3745105996ce96c4535134ed3c55d936d9
jigme1968 <jigme1968@gmail.com>
2020-11-11 15:11:29 +0800

    Initial Commit

---
+++ HospitalAnalysis/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HospitalAnalysis
+//
+//  Created by jk on 2020/11/11.
+//  Copyright © 2020 ssl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
